bd6b5451092d87264ce79f266220edd8e1bb6b15
Mayur Maskar <mayurmaskar007@gmail.com>
2025-08-10 22:06:16 +0530

    For testing all Environment are same

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://chatsapp-backend-hom7.onrender.com/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'localhost',
+  apiUrl: 'https://chatsapp-backend-hom7.onrender.com/api',
 };
